ca57848d282ab5f25c7918cd100b394e1ade2ba4
safarov-e <elkhan.safarov10@yandex.com>
2021-02-04 18:19:19 +0500

    Переход по директориям

--- file-manager/src/components/readdir.vue
+++ file-manager/src/components/readdir.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <ul>
       <li v-for="(item, index) in directory" :key="index">
-        <a :href="item.file">{{item.file}}</a>
+        <a :href="item.file" @click.prevent="nextFolder(item.file)">{{item.file}}</a>
         <p>{{item.size}}</p>
         <p>{{item.birthtime | fileDate}}</p>
       </li>
@@ -11,19 +11,32 @@
 </template>
 
 <script>
-import {mapGetters, mapActions} from 'vuex'
 import './expansion.css'
 import './readdir.css'
 export default {
-  computed: mapGetters(['directory']),
-  methods: mapActions(['fetchDirectory']),
+  data() {
+    return {
+      directory: []
+    }
+  },
+  computed: {},
+  methods: {
+    nextFolder(value) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://localhost:3001/routes/folder", true);
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.send("param=" + JSON.stringify({ name: value }));
+    }
+  },
   filters: {
     fileDate(value) {
       return value.replaceAll('-', '.').replaceAll('T', ' ').slice(0, 19)
     }
   },
   async mounted() {
-    this.fetchDirectory()
+    const res = await fetch('http://localhost:3001/routes')
+    const paths = await res.json()
+    this.directory = paths
   }
 }
 </script>
